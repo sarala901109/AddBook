af2b2198bd45027b7f1bcef4bf572a8f09401e32
sarala901109 <sarala.r@kgcas.com>
2016-11-10 13:23:23 -0800

    Added search button

---
+++ AddBook-master/build-address-book-Android_for_armeabi_v7a_GCC_4_9_Qt_5_7_0-Debug/android-build/bin/AndroidManifest.xml.d
@@ -0,0 +1,2 @@
+D:\gitintro\AddBook-master\build-address-book-Android_for_armeabi_v7a_GCC_4_9_Qt_5_7_0-Debug\android-build\bin\AndroidManifest.xml : \
+D:\gitintro\AddBook-master\build-address-book-Android_for_armeabi_v7a_GCC_4_9_Qt_5_7_0-Debug\android-build\AndroidManifest.xml \
